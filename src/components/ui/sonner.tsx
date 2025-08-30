1038e176b653780ce0305c3667347a4e830a2c85
blackboxai-deploy <deploy@blackboxai.dev>
2025-08-31 03:03:43 +0530

    Add src/components/ui/sonner.tsx

---
+++ src/components/ui/sonner.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export function Toaster() {
+  return (
+    <div id="toast-container" className="fixed top-4 left-4 z-50">
+      {/* Placeholder for toast notifications */}
+    </div>
+  )
+}
